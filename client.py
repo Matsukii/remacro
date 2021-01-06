441e500ace2961f034c1b5800f68ad0b10f56154
Matsukii <matheuscesar2000@gmail.com>
2021-01-06 15:28:48 -0300

    : profiles, better way to set keys and more actions     - profiles for current window!     - Json file load for profiles     - better way to set keys, no need to set name and use functions manually     - better documentation i guess     - more actions         - run cli commands         - mouse click and reposition     - "imaspeeeeed" - speed should increase a bit

--- client.py
+++ client.py
@@ -4,25 +4,41 @@ import socketio
 import _thread as thread
 import threading
 import sys
+import subprocess
+import json
 
 
 # https://pythonhosted.org/pynput/mouse.html
 import pynput
-from pynput.keyboard import Key, Controller as keyboardController
+from pynput.keyboard import Key, KeyCode as keyCode, Controller as keyboardController
 from pynput.mouse    import Button as mouseBtn, Controller as mouseController
 from win32gui        import GetWindowText, GetForegroundWindow
 
 
 
 
-# socket server URL, change to ip of your pc on your local networks
-# if opted to use locally ex: http://192.168.0.1:3001 or http://localhost:3001
-#
-# Also, is possible to change trough CLI, add --host <socket_server_url>
-# * ex: --host http://localhost:3001
-# * the default url is "https://remacro.herokuapp.com"
-socketURL = 'https://remacro.herokuapp.com'
 
+
+
+options = {
+    # socket server URL, change to ip of your pc on your local networks
+    # if opted to use locally ex: http://192.168.0.1:3001 or http://localhost:3001
+    #
+    # Also, is possible to change trough CLI, add --host <socket_server_url>
+    # * ex: --host http://localhost:3001
+    # * the default url is "https://remacro.herokuapp.com"
+    # "socketURL": 'https://remacro.herokuapp.com',
+    "socketURL": 'http://localhost:3001',
+
+    # enable default actions for any application
+    "enabledefault": True,
+
+    # use profiles provided in a json file
+    # set the path to the file or keep it as False
+    # * ex: "C:/path/to/my-profiles.json"
+    # --usekeyfile "C:/path/to/my-profiles.json"
+    "usekeyfile": False,
+}
 
 # CL arguments
 try:
@@ -31,24 +47,98 @@ try:
 
         # --host <host>
         if(sys.argv[i] == "--host"):
-            print("socket io host: " + sys.argv[i+1])
+            print("[ARGV] socket io host: " + sys.argv[i+1])
             socketURL = sys.argv[i+1]
         
+        # --enabledefault
+        if(sys.argv[i] == "--enabledefault"):
+            print("[ARGV] [FLAG] default actions enabled")
+            socketURL = sys.argv[i+1]
+        
+        # --usekeyfile
+        if(sys.argv[i] == "--usekeyfile"):
+            if(sys.argv[i+1] == "true"):
+                options["usekeyfile"] = "profiles.json"
+                print("[ARGV] [FLAG] using profiles from json, path: " + sys.argv[i+1])
+            elif(sys.argv[i+1].startswith('-') == False):
+                options["usekeyfile"] = sys.argv[i+1]
+                print("[ARGV] [VALUE] using profiles from json, path: " + sys.argv[i+1])
+                
+
         # --help / -h
         if(sys.argv[i] == '--help' or sys.argv[i] == '-h'):
+            print("--help or -h | show this help and stop the script")
+            print("--enabledefault | enable default actions for any application")
             print("--host <host> | set socket.io server host")
-            print("\r ex: --host http://localhost:3001")
+            print("\t ex: --host http://localhost:3001")
+            print("--usekeyfile <true|file> | use profiles from json file")
+            print("\n\t MOUSECLICK AND MOUSEMULTICLICKS IS NOT SUPPORTED WITH A FILE \n")
+            print("\t enable with file on client.py folder with profiles.json name")
+            print("\t ex: --usekeyfile true")
+            print("\t enable with file with a custom path")
+            print('\t ex: --usekeyfile "C:/path/to/my/profiles.json"')
+            exit()
 
         i+=1
     pass
 except IndexError:
-    print('no arguments')
+    print('[ERR] no arguments')
     pass
+
+
+# SET your keys and profiles here, following the template.json
+# 
+profiles = {
+    "DEFAULT": {
+        "name": "ACTIONS_DEFAULT",
+        "windowName": "",
+        "enable": options["enabledefault"],
+        "desc": "if no app is available and is enable, these actions will be executed",
+        "A7": {
+            "name": "New tab",
+            "type": "pressKeys",
+            "data": [
+                Key.ctrl,
+                "t"
+            ]
+        },
+        "A8": {
+            "name": "Close tab",
+            "type": "pressKeys",
+            "data": [
+                Key.ctrl,
+                "w"
+            ]
+        },
+        
+        "A9": {
+            "name": "open github",
+            "type": "openURL",
+            "data": "https://github.com"
+        },
+    },    
+}
+
+
+
+if(options["usekeyfile"] != False):
+    try:
+        print(options["usekeyfile"])
+        keysFile = open(options["usekeyfile"], 'r')
+        profiles = json.load(keysFile)
+        keysFile.close()
+        print("[LOAD] profiles loaded from json")
+        pass
+    except FileNotFoundError as identifier:
+        print("[ERR] profiles file not founded, using profiles from client.py")
+        pass
+
+
 
 
 sio = socketio.Client()
 def tryConnect():
-    sio.connect(socketURL)
+    sio.connect(options["socketURL"])
 
 try:
     tryConnect()
@@ -74,7 +164,25 @@ def kepprun():
             print("stopp")
             stop = False
 
+# get profile window names
+def getWindowNames(profiles):
+    names = []
 
+    for i in profiles.keys():
+        if(i != "test" and i != "template" and i != "DEFAULT"):
+            names.append({
+                "window": profiles[i]["windowName"],
+                "profile": i
+            })
+
+    return names
+
+
+# set window names on a variable
+windowNames = getWindowNames(profiles)
+
+keyboard = keyboardController()
+mouse = mouseController()
 
 # encapsulte keypress and type for easier use
 # press and release a single key
@@ -92,6 +200,16 @@ def pressKeys(keyArr):
     for key in keyArr:
         keyboard.release(key)
 
+# [Integer key codes] press multiple keys from an array of keys
+def pressKeyCodes(keyArr):
+    global keyboard
+    for key in keyArr:
+        keyboard.press(keyCode(key["keyCode"]))
+
+    for key in keyArr:
+        keyboard.release(keyCode(key["keyCode"]))
+
+
 # type some text
 def ttype(text):
     keyboard = keyboardController()
@@ -103,68 +221,103 @@ def ttype(text):
 def openURL(url):
     webbrowser.open(url)
 
+# set X and Y postions of the cursor
+def mouseSetPosition(coords):
+    global mouse
+    mouse.position = (int(coords[0]), int(coords[1]))
+
+# move relative to current postion
+def mouseMove(coords):
+    global mouse
+    mouse.move(int(coords[0]), int(coords[1]))
+
+# press all mouse buttons on array and then release
+def mouseMultiClicks(buttons):
+    for btn in buttons:
+        mouse.press(btn)
+
+    for btn in buttons:
+        mouse.release(btn)
+
+# simple click with count
+def mouseClick(button):
+    global mouse
+    mouse.click(button.button, button.times)
+
+# dict of action types
+actionTypes = {
+    "pressKey": pressKey,
+    "pressKeys": pressKeys,
+    "pressKeyCodes": pressKeyCodes,
+    "typeText": ttype,
+    "openURL": webbrowser.open,
+    "runCommand":subprocess.Popen,
+    "mouseMove": mouseMove,
+    "mouseSetPosition": mouseSetPosition,
+    "mouseMultiClicks": mouseMultiClicks,
+    "mouseClick": mouseClick,
+}
 
 
-
-# add the button names here and use buttons[index]
-# used to remote user get available buttons
-# ! if is already started, need to close and start again
-buttons = [
-    'Alt tab',
-    'open Github',
-]
 
 def keyHandler(key):
-    currentWindow = GetWindowText(GetForegroundWindow())
-    keyboard = keyboardController()
-    mouse = mouseController()
-    
+    global profiles
+    global windowNames
 
-    print("executing: " + key)
-    if(key == buttons[0]):
-        pressKeys([Key.alt, Key.tab])
+    def execute(key):
+        currWindow = GetWindowText(GetForegroundWindow())
 
-    
-    elif(key == buttons[1]):
-        openURL("https://github.com")
+        profile = "DEFAULT"
+        for i in windowNames:
+            if(currWindow.find(i["window"]) > -1):
+                profile = i["profile"]
+                break
+        
+
+        profile = profiles[profile]
+        if(key in profile.keys()):
+            action  = profile[key]
+
+            if(profile["enable"]):
+                actionTypes[action["type"]](action["data"])
+                print("[BACT] " + key + "/" + currWindow + ": " + action["name"])
+            else:
+                print("[INFO] actions of '" + profile["name"] + "' are disabled")
 
 
-    # examples
 
-    # BASIC - copy when a 
-    # if(key == "<MyButton>"):
-    #     print("executing MyButton 1 : copy")
-    #     pressKey([Key.ctrl, 'c'])
+    if("button" in key):
+        # print("executing: " + key["button"])
+        # print(str(key["button"]))
+        execute(key["button"])
 
-    # SPECIFIC WINDOW - open new tab only if chrome browser is open in foreground
-    # if(currentWindow.find("Chrome>") > -1):
-    #     if(key == buttons[1]):
-    #         pressKeys([Key.ctrl, 'n'])
-    
-
-    # if does not match any button name
     else:
-        print("invalid key/not found")
+        print("[ERR] key not found")
     
+    
+
 
 
 @sio.event
 def connect():
     print("Connected")
 
+
 @sio.event
 def sid(user):
-    print("Your ID: " + user['id'] + " @ timestamp:" + str(user['conAt']))
+    global options
+    options["userID"] = user["id"]
+    print("Your ID: " + user['id'] + " | connected@timestamp: " + str(user['conAt']))
     print("use the id in web UI to send button press with the name")
-    print("or click here: "+ socketURL + "/" + user['id'])
+    print("or click here: "+ options["socketURL"] + "/" + user['id'])
 
 @sio.event
 def macro(key):
-    keyHandler(key['key'])
+    keyHandler(key)
 
 @sio.event
 def getBtns(data):
-    sio.emit("resBtns", {'toid': data['toid'], 'btns':buttons})
+    sio.emit("resBtns", {'toid': data['toid'], 'profiles':str(profiles)})
     
 
 
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # remacro
-
+Remote macro keys trough websockets
 
 [Online Remote control page](https://remacro.herokuapp.com)
 
@@ -19,7 +19,7 @@ This is part of the [sMacros](https://github.com/Matsukii/sMacros), the idea cam
 
 _"So, can you install python and run this (not-suspicious) script and let me control the key combinations you set from internet?"_ - Ehhh..., no?
 
-Using localy should be fine, using the socket server online may have security issues, however, the host uses https, the generated id on client **is not** saved to any database/file (code availabe at ['rio' branch](https://github.com/Matsukii/remacro/tree/rio)) and when the client reconnect the code changes
+Using localy should be fine, using the socket server online may have security issues, however, the host uses https, the generated id on client **is not** saved to any database/file (code availabe at ['rio' branch](https://github.com/Matsukii/remacro/tree/rio) or rio folder) and when the client reconnect the code changes
 
 
 
@@ -28,10 +28,50 @@ Using localy should be fine, using the socket server online may have security is
 For both options is required to have python (3.7+ recommended) and pip to install dependencies (pynput, thread, socketio...) <br>
 For local socket.io server is required to have node.js and npm setted up
 
-download and open rMacro.py with a text editor, create your own key events
+download and open client.py with a text editor, create your own key events or use a profile.json file as detailed further
 
 
-### Add buttons:
+## Options
+
+all can be set from command line
+
+use -h or --help to view all comnands (the script will not be executed entirely)
+
+tip: create a shortcut to the client.py, and change the destiny with options to execute with python in a dlouble click or open with
+
+```py
+
+options = {
+    # socket server URL, change to ip of your pc on your local networks
+    # if opted to use locally ex: http://192.168.0.1:3001 or http://localhost:3001
+    #
+    # Also, is possible to change trough CLI, add --host <socket_server_url>
+    # * ex: --host http://localhost:3001
+    # * the default url is "https://remacro.herokuapp.com"
+    # "socketURL": 'https://remacro.herokuapp.com',
+    "socketURL": 'http://localhost:3001',
+
+    # enable default actions for any application
+    "enabledefault": True,
+
+    # use profiles provided in a json file
+    # set the path to the file or keep it as False
+    # * ex: "C:/path/to/my-profiles.json"
+    # --usekeyfile "C:/path/to/my-profiles.json"
+    "usekeyfile": False,
+}
+```
+
+## Add profiles and buttons:
+
+_If you want to save the profiles to a .json file, use pressKeyCodes instead of pressKeys (detailed below) and set the "useFile" on options flag to True or add --usekeyfile [<path/to/profiles.json> or true] in command line._
+
+_*IMPORTANT: mouseClick and mouseMultiClicks is not supported with .json file*_
+
+Key codes:
+
+- [here is a list of keyCodes](https://gist.github.com/Matsukii/fbd15c688e39e2033d244e939b3aa7a5)
+- [also try out keycode.info](https://keycode.info)
 
 ```py
 
@@ -39,36 +79,67 @@ download and open rMacro.py with a text editor, create your own key events
 #
 # these are the names that will be sent to web UI to create
 # a list of buttons and sent back to client when a button is clicked
-buttons = [
-    'button1',      # index: 0
-    'MyNewButton'   # index: 1
+profiles = [
+    "MYPROFILENAME": {
+        "name": "custom profile",
+        "windowName": "Chrome",
+        "desc": "macro keys for Google Chrome",
+
+        # True or False to enable/disable
+        "enable": True,
+
+        # buttons
+        "A7": {
+            "name": "New tab",
+            
+            # see template.json to view all types os actions as example
+            "type": "pressKeys",
+            
+            # depend on type, for pressKeys is a array, for pressKey is only the key
+            "data": [
+                # Key.<your_key> to use keys like ctrl, alt, shift... 
+                Key.ctrl,
+                "t"
+            ]
+        },
+        "A8": {
+            "name": "Close tab",
+            "type": "pressKeys",
+            "data": [
+                Key.ctrl,
+                "w"
+            ]
+        },
+        
+        # if using json file for profiles:
+        "A9": {
+            "name": "Close tab",
+            "type": "pressKeyCodes",
+            "data": [
+                {
+                    "key": "ctrl",
+                    "keyCode": 17,
+                    "isModifier": true
+                },
+                {
+                    "key": "w",
+                    "keyCode": 87
+                }
+            ]
+        },
+        
+        "B0": {
+            "name": "open github",
+            "type": "openURL",
+            "data": "https://github.com"
+        },
+    },
+
 ]
-
-
-# then go to
-def keyHandler(key):
-# and add a condition using the index of the button name you want to use
-# * You shoul use only one button for one action in general cases,
-# * or it will execute both of your key presses when you click
-
-def keyHandler(key):
-
-    # when the button coming from web is equal to button in position 0 (button1),
-    # will execute alt+tab
-    if(key == buttons[0]):
-        pressKeys([Key.alt, Key.tab])
-    
-    # if key name is equal to 'MyNewButton', press Windows or command(Mac) key
-    elif(key == buttons[1]):
-        pressKey(Key.cmd)
-
-    else:
-        print(key + "action is not defined")
 ```
 
 
-encapsulated functions examples:
-
+## Encapsulated functions and some examples:
 ```py
 # there is some functios to make easies to use,
 # it just encapsulate from keyboard and mouse
@@ -87,8 +158,7 @@ pressKeys([Key.ctrl, 'c'])
 
 # This following may NOT work, the function loops trough
 # the array and press all of them then loop again releasing
-
-# basically will be like press ctrl+c+v
+# basically will should like press ctrl+c+v
 pressKeys([Key.ctrl, 'c', Key.ctrl, 'v'])
 
 # esc, will press a single key and release
@@ -102,9 +172,29 @@ openURL("https://github.com")
 # type some text
 ttype("hi")
 
+# other available types in dictionary:
+actionTypes = {
+    "pressKey": pressKey,
+    "pressKeys": pressKeys,
+
+    # use integer keyCode to press the keys, the profiles can be saved in a json file
+    # but need a structure like the following example in "data":[] array of pressKeyCodes type:
+    # ex: data: [ {"key": "ctrl", "keyCode": 17, "isModifier": true}, {"key": "t", "keyCode": 84} ] 
+    "pressKeyCodes": pressKeyCodes,
+    
+    "typeText": ttype,
+    "openURL": webbrowser.open,             # open a URL on default browser
+    "runCommand":subprocess.Popen,          # run on cmd 
+    "mouseMove": mouseMove,                 # relative to current position: mouseMove([X, -Y]) -> mouseMove([10, -5])
+    "mouseSetPosition": mouseSetPosition,   # mouseSetPosition([X, Y])
+    "mouseMultiClicks": mouseMultiClicks,   # mouseMultiClicks([mouseBtn.rigth, mouseBtn.left])
+    "mouseClick": mouseClick,               # mouseClick({"button": mouseBtn.left, "times": 2}) -> a double left click
+}
+
+
 ```
 
-### You can find more about pynput here:
+## You can find more about pynput here:
 This project uses pynput, check out the [dev repository](https://github.com/moses-palmer/pynput)
 
 - [Pynput](https://pythonhosted.org/pynput/)
@@ -113,7 +203,9 @@ This project uses pynput, check out the [dev repository](https://github.com/mose
 - [Limitations](https://pynput.readthedocs.io/en/latest/limitations.html)
 
 
-### Run
+## Run
+
+_If using devices on your network, you may need to open the port on windows firewall_
 
 If you wnt to use locally on your network, set 'socketURL' variale to IP of the device where its running 
 ```ex: http://localhost:3001 or http://192.168.0.1:3001```
@@ -128,4 +220,24 @@ _If using locally, install dependencies with `npm install` and start the server 
 After start and connect to socket.io server the ID and a url with ID will be printed
 
 Open the url or copy the code and go to `https://remacro.herokuapp.com` _localhost:3001 on local_, paste the code and click start and you are ready to use
+
+## Changelog
+* 2020/Aug/09
+    - Created and pushed 
+
+* 2021/Jan/06
+    - profiles for current window!
+    - Json file load for profiles
+    - better way to set keys, no need to set name and use functions manually
+    - better documentation i guess
+    - more actions
+        - run cli commands
+        - mouse click and reposition
+    - "imaspeeeeed" - speed should increase a bit
+
+<br>
+<br>
+
+# Licence: MIT
+
 
